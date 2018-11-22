32d778c51eb129698bc36b31d04e47498e9af770
Robert Evans <robertevans@robertevansmb.com>
2018-11-22 13:37:44 -0500

    Complete events challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -47,6 +47,12 @@ var count = 0;
 var addOne = function addOne() {
     console.log('addOne');
 };
+var minusOne = function minusOne() {
+    console.log('minusOne');
+};
+var reset = function reset() {
+    console.log('reset');
+};
 
 var templateTwo = React.createElement(
     'div',
@@ -61,6 +67,16 @@ var templateTwo = React.createElement(
         'button',
         { onClick: addOne },
         '+1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: minusOne },
+        '-1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: reset },
+        'reset'
     )
 );
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -22,12 +22,20 @@ let count = 0;
 
 const addOne = () => {
     console.log('addOne');
+};
+const minusOne = () => {
+    console.log('minusOne');
+};
+const reset = () => {
+    console.log('reset');
 }
 
 const templateTwo = (
     <div>
         <h1>Count: {count}</h1>
         <button onClick={addOne}>+1</button>
+        <button onClick={minusOne}>-1</button>
+        <button onClick={reset}>reset</button>
     </div>
 )
 const appRoot = document.getElementById('app');
