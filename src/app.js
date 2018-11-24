791b640754f5c3cbd0516fdc40412d17f5eb5593
Robert Evans <robertevans@robertevansmb.com>
2018-11-24 14:05:40 -0500

    Start on react components

---
+++ src/app.js
@@ -0,0 +1,29 @@
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
+    }
+}
+
+const jsx = (
+    <div>
+        <Header/>
+        <Action/>
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
