acc28cf0b8866338ff85fa63a16a5e59067d99ed
Robert Evans <robertevans@robertevansmb.com>
2018-11-24 13:33:43 -0500

    Finish ES6 Classes 1 Challenge

--- public/scripts/playground/es6-classes-1.js
+++ public/scripts/playground/es6-classes-1.js
@@ -7,26 +7,35 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 var Person = function () {
     function Person() {
         var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
         _classCallCheck(this, Person);
 
         //es6 allows defaults
         this.name = name;
+        this.age = age;
     }
 
     _createClass(Person, [{
         key: 'getGreeting',
         value: function getGreeting() {
             //return 'Hi. I am ' + this.name + '!';
-            return 'Hi. I am ' + this.name + '!';
+            return 'Hi. I am ' + this.name + '!'; //template string uses backticks (`)
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age + ' year(s) old.';
         }
     }]);
 
     return Person;
 }();
 
-var me = new Person('Robert Evans');
+var me = new Person('Robert Evans', 29);
 console.log(me.getGreeting());
+console.log(me.getDescription());
 
 var other = new Person();
 console.log(other.getGreeting());
+console.log(other.getDescription());
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,16 +1,22 @@
 
 class Person {
-    constructor(name = 'Anonymous') { //es6 allows defaults
+    constructor(name = 'Anonymous', age = 0) { //es6 allows defaults
         this.name = name;
+        this.age = age;
     }
     getGreeting(){
         //return 'Hi. I am ' + this.name + '!';
-        return `Hi. I am ${ this.name }!`;
+        return `Hi. I am ${ this.name }!`; //template string uses backticks (`)
+    }
+    getDescription(){
+        return `${this.name} is ${this.age} year(s) old.`;
     }
 }
 
-const me = new Person('Robert Evans');
+const me = new Person('Robert Evans', 29);
 console.log(me.getGreeting());
+console.log(me.getDescription());
 
 const other = new Person();
 console.log(other.getGreeting());
+console.log(other.getDescription());
