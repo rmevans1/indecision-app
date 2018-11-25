a88ec8e925b09289ffcef44a335df1e57c1aa65b
Robert Evans <robertevans@robertevansmb.com>
2018-11-25 10:28:14 -0500

    Add big button styling

--- src/components/Action.js
+++ src/components/Action.js
@@ -5,6 +5,7 @@ const Action = (props) => (
         <button
             disabled={!props.hasOptions}
             onClick={props.handlePick}
+            className="big-button"
         >
             What should I do?
         </button>
