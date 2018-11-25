bcb96013149071f90dedbd0d1da1976c0e99a6ce
Robert Evans <robertevans@robertevansmb.com>
2018-11-24 23:12:46 -0500

    Finish named export challenge

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 // import '.\\utils.js';
 import { square, add } from '.\\utils.js';
+import { isAdult, canDrink } from '.\\person.js';
 
 console.log('app.js is running');
 console.log(square(4));
 console.log(add(100, 23));
+
+console.log('Is Adult: ', isAdult(18));
+console.log('Is Adult: ', isAdult(7));
+console.log('Can Drink: ', canDrink(21));
+console.log('Is Adult: ', isAdult(9));
