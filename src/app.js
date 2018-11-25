c54ca5166290ddf837e733b37f2a740dfcdb6a7f
Robert Evans <robertevans@robertevansmb.com>
2018-11-24 23:28:59 -0500

    Finished default exports challenge

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 // import '.\\utils.js';
 import subtract, { square, add } from '.\\utils.js';
-import { isAdult, canDrink } from '.\\person.js';
+import isSenior, { isAdult, canDrink } from '.\\person.js';
 
 console.log('app.js is running');
 console.log(square(4));
@@ -10,4 +10,6 @@ console.log(subtract(100, 81));
 console.log('Is Adult: ', isAdult(18));
 console.log('Is Adult: ', isAdult(7));
 console.log('Can Drink: ', canDrink(21));
-console.log('Is Adult: ', isAdult(9));
+console.log('Can Drink: ', canDrink(9));
+console.log('Is Senior: ', isSenior(66));
+console.log('Is Senior: ', isSenior(25));
