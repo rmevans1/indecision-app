879384de9b27c212b7ec547d17fa39229e6a04fb
Robert Evans <robertevans@robertevansmb.com>
2018-11-25 11:25:12 -0500

    Style modal body

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -10,9 +10,9 @@ const OptionModal = (props) => (
             closeTimeoutMS={200}
             className="modal"
         >
-            <h3>Selected Option</h3>
-            {props.selectedOption && <p>{props.selectedOption}</p>}
-            <button onClick={props.handleCloseModal}>Okay</button>
+            <h3 className="modal__title">Selected Option</h3>
+            {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+            <button  className="button" onClick={props.handleCloseModal}>Okay</button>
         </Modal>
 );
 
