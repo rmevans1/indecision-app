94a76f6cd2f71939a276430d88780860417aa3cc
Robert Evans <robertevans@robertevansmb.com>
2018-11-24 23:36:00 -0500

    Test validator plugin

--- src/app.js
+++ src/app.js
@@ -1,15 +1,3 @@
-// import '.\\utils.js';
-import subtract, { square, add } from '.\\utils.js';
-import isSenior, { isAdult, canDrink } from '.\\person.js';
+import validator from 'validator';
 
-console.log('app.js is running');
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 81));
-
-console.log('Is Adult: ', isAdult(18));
-console.log('Is Adult: ', isAdult(7));
-console.log('Can Drink: ', canDrink(21));
-console.log('Can Drink: ', canDrink(9));
-console.log('Is Senior: ', isSenior(66));
-console.log('Is Senior: ', isSenior(25));
+console.log(validator.isEmail('test@gmail.com'));
