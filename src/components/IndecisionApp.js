c9cb6e23f921f678c3fb32c1ca683781dadc763e
Robert Evans <robertevans@robertevansmb.com>
2018-11-25 01:06:00 -0500

    Pull event handlers into class properties

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,16 +5,33 @@ import Options from "./Options";
 import AddOption from "./AddOption";
 
 class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.state = {
-            options: props.options
+    state = {
+        options: []
+    };
+    handleDeleteOptions = () => {
+        this.setState(() => ({ options: [] }));
+    };
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option)=> optionToRemove !== option )
+        }));
+    };
+    handlePick = () => {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+        alert(option);
+    };
+    handleAddOption = (option) => {
+        if (!option) {
+            return 'Enter valid value to add item';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists';
         }
-    }
+
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option)
+        }));
+    };
     // Life Cycle Method- Fires on component mount
     componentDidMount() {
 
@@ -40,30 +57,6 @@ class IndecisionApp extends React.Component {
     //    Useful for page change
     componentWillUnmount(){
         console.log('componentWillUnmount!');
-    }
-    handleDeleteOptions() {
-        this.setState(() => ({ options: [] }));
-    }
-    handleDeleteOption(optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option)=> optionToRemove !== option )
-        }));
-    }
-    handlePick() {
-        const randomNum = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[randomNum];
-        alert(option);
-    }
-    handleAddOption(option) {
-        if (!option) {
-            return 'Enter valid value to add item';
-        } else if (this.state.options.indexOf(option) > -1) {
-            return 'This option already exists';
-        }
-
-        this.setState((prevState) => ({
-            options: prevState.options.concat(option)
-        }));
     }
     render() {
         const subtitle = 'Put your life in the hands of a computer.';
